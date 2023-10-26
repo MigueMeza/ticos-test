27221fdb721e40d24f8b646b6007dcfb211109ad
MigueMeza <miguelangelmezavazquez24@hotmail.com>
2023-10-26 07:09:37 -0600

    final changes

--- deviceFarm2.sh
+++ deviceFarm2.sh
@@ -2,15 +2,20 @@
 
 # Variables
 PROJECT_ARN="arn:aws:devicefarm:us-west-2:480127788555:project:0350d16f-da9c-43f7-a14c-dc25fb82c8b5"
-DEVICE_POOL_ARN="arn:aws:devicefarm:us-west-2:723718931296:devicepool:bb57733a-a935-4cfd-b9f3-e9ac685585fe/0cf4cfb6-3554-4bf7-804a-6f7ce0a0cb6a"
-APP_PATH="test.apk"
+#test device pool
+DEVICE_POOL_ARN="arn:aws:devicefarm:us-west-2:480127788555:devicepool:0350d16f-da9c-43f7-a14c-dc25fb82c8b5/3d156911-a086-4a8c-aaea-55853c012a6d"
+#DEVICE_POOL_ARN="arn:aws:devicefarm:us-west-2:480127788555:devicepool:0350d16f-da9c-43f7-a14c-dc25fb82c8b5/e35b0bd1-9657-4f78-b44b-99dad91d96ea"
+APP_PATH="aeromexicoapp.ipa"
 TEST_SPEC_PATH="./testSpec.yaml"
 echo "ok1"
 
 # Subir la aplicación
-app_upload=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name $APP_PATH --type ANDROID_APP --project-arn $PROJECT_ARN --output text)
-echo $app_upload
-echo "ok2"
+#app_upload_result=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name $APP_PATH --type IOS_APP --project-arn $PROJECT_ARN)
+#app_upload=$(echo $app_upload_result | jq -r '.upload.arn')
+#echo $app_upload
+#echo "ok2"
+#rslt=$(aws devicefarm get-upload --arn $app_upload)
+#echo $rslt
 
 # Crear una especificación de pruebas (reemplaza 'tu_especificacion_de_pruebas.yaml' con el nombre real del archivo)
 #test_spec_arn=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name "MyTestSpec" --type APPIUM_JAVA_TESTNG_TEST_SPEC --query 'upload.arn' --output text)
@@ -22,25 +27,34 @@ echo "ok2"
 #echo "ok3"
 
 # Iniciar la ejecución de pruebas
-aws devicefarm schedule-run \
+run_result=$( aws devicefarm schedule-run \
     --project-arn "$PROJECT_ARN" \
-    --app-arn "$app_upload" \
+    --app-arn "arn:aws:devicefarm:us-west-2:480127788555:upload:0350d16f-da9c-43f7-a14c-dc25fb82c8b5/4694ebb3-50fe-47d9-959d-28b77fe544a5" \
     --device-pool-arn "$DEVICE_POOL_ARN" \
     --name "MyRun" \
-    --test '{"type": "BUILTIN_FUZZ"}'
+    --test '{"type": "BUILTIN_FUZZ"}')
 echo "ok4"
 
-# Esperar a que las pruebas finalicen
+# Extrae el ARN de la ejecución de prueba
+run_arn=$(echo $run_result | jq -r '.run.arn')
+echo "ok5"
+
+# Espera hasta que la ejecución de prueba finalice
 while true; do
-    status=$(aws devicefarm get-run --arn "$session_arn" --query 'run.status' --output text)
-    if [ "$status" == "COMPLETED" ]; then
+    status=$(aws devicefarm get-run --arn $run_arn --query "run.status" --output text)
+    if [[ $status == "COMPLETED" ]]; then
+        echo "Run completed successfully."
         break
-    elif [ "$status" == "ERRORED" ] || [ "$status" == "STOPPED" ]; then
-        echo "Error al ejecutar pruebas"
+    elif [[ $status == "ERRORED" || $status == "STOPPED" ]]; then
+        echo "Run encountered an error or was stopped."
         exit 1
+    else
+        echo "Waiting for the run to finish..."
+        sleep 10  # Adjust the polling interval as needed (in seconds)
     fi
-    sleep 60
 done
+echo "ok6"
+
 
 # Obtener los resultados de las pruebas
 results=$(aws devicefarm get-run --arn "$session_arn" --query 'run.result' --output text)
